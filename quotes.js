4ddf9a099d1f741cca36a2d572b4d41829336e0b
Kevin Reiner <kevinreiner@MBP-von-Kevin.fritz.box>
2024-12-23 10:15:30 +0100

    initial commit

---
+++ quotes.js
@@ -0,0 +1,49 @@
+const localQuotes = [
+    {
+        quote: "Design is not just what it looks like and feels like. Design is how it works.",
+        author: "Steve Jobs"
+    },
+    {
+        quote: "If you think good design is expensive, you should look at the cost of bad design.",
+        author: "Ralf Speth"
+    },
+    {
+        quote: "People ignore design that ignores people.",
+        author: "Frank Chimero"
+    },
+    {
+        quote: "The details are not the details. They make the design.",
+        author: "Charles Eames"
+    },
+    {
+        quote: "Good design is obvious. Great design is transparent.",
+        author: "Joe Sparano"
+    },
+    {
+        quote: "A user interface is like a joke. If you have to explain it, it’s not that good.",
+        author: "Martin LeBlanc"
+    },
+    {
+        quote: "The role of the designer is that of a good, thoughtful host anticipating the needs of his guests.",
+        author: "Charles Eames"
+    },
+    {
+        quote: "It’s not about what it looks like or feels like. It’s about how it works.",
+         author: "Steve Jobs"
+    },
+    {
+        quote: "Usability is about people and how they understand and use things, not about technology.",
+        author: "Steve Krug"
+    },
+    {
+        quote: "Content precedes design. Design in the absence of content is not design, it’s decoration.",
+        author: "Jeffrey Zeldman"
+    },
+
+    {
+        quote: "Design doesn’t mean making things pretty; it’s the exact opposite of being random.",
+        author: ""
+    },
+
+  ];
+  
---
+++ script.js
@@ -0,0 +1,80 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// let apiQuotes = [];
+
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show New Quote
+// function newQuote() {
+    // Get Quotes From API
+//    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+//    console.log(quote);
+// }
+
+// Get Quotes From API
+// async function getQuotes() {
+//    loading();
+//    const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+//    try {
+//        const response = await fetch(apiURL);
+//        apiQuotes = await response.json();
+//        newQuote();
+//    } catch (error) {
+        // Catch Error Here
+//    }
+// }
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - {authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+// getQuotes();
+
+// Show New Quote
+function newQuote() {
+    loading();
+    // Pick a random quote from the quotes.js file
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+
+    // Check if Author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+    
+    // Check Quote length to determine styling
+    if (quote.quote.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+   // Set Quote, Hide Loader
+    quoteText.textContent = quote.quote;
+    complete();
+}
+
+
+newQuote();
